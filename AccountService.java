df2bb4675b845239a1c4b2ff04de15e315cee42a
Guilain3 <153222631+Guilain3@users.noreply.github.com>
2023-12-24 00:13:36 +0200

    Add files via upload

---
+++ AccountService.java
@@ -0,0 +1,17 @@
+
+package Service;
+
+import Model.Accounts;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+
+public interface AccountService extends Remote{
+    Accounts recordAccounts(Accounts accobj) throws RemoteException;
+    Accounts modifyAccounts(Accounts accobj) throws RemoteException;
+    Accounts removeAccounts(Accounts accobj) throws  RemoteException;
+    Accounts findAccounts(Accounts accobj) throws RemoteException;
+    List<Accounts> Accountsall() throws RemoteException;
+    //public List<Accounts> retrieveUser(String staffid, String password) throws RemoteException;
+}
